e8c15e75864b4eeec72554b38286c5c50cacb233
C4rtex <carlos.jimenezc@cun.edu.co>
2021-09-07 15:31:18 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const typed = new Typed('.typed', {
+    strings: ['<i class="amarllo" >Chimba<i>',
+        '<i class="verde" >Confiable<i>',
+        '<i class="azul" >Amable<i>',
+        '<i>Llameme ;)<i>'
+    ],
+
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    backDelay: 500,
+    contentType: 'html',
+});
